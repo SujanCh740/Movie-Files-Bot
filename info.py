ed3d6b15fd31e2863d9d806590577bab47ef490f
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2025-12-22 20:16:49 +0545

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MoviesFilesBot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Sujan_Ch")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
