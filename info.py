c36c474b78ea8b7f363a5d0255e034f74d27ea0a
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2026-03-26 09:00:06 +0545

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Sujan_Ch")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TelegramFiles')
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
+VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
 HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/How_2_Download_From_TeraBox/5732') # How to open tutorial link for verification
 
 # NEW: WebApp URL for verification (host your verify_webapp.html here)
