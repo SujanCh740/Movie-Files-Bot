4c81ef94ec2e313bc752e77144e2ed16bce95e07
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2026-03-26 07:50:16 +0545

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -19,7 +19,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("BenFilterBot")
+    return web.json_response("Your_Movie_Search_Bot")
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
