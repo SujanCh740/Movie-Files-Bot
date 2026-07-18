66d343cb0ce45019f7b3557541b30ddea3416d79
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2026-07-18 14:52:46 +0545

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://iili.io/fmFvFN1.jpg'))
 CODE = (environ.get('CODE', 'https://iili.io/ftvUziv.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'shortxlinks.com'))
-STREAM_API = (environ.get('STREAM_API', '21312ded78e50e34e58391be04bf4a266e6b5d8f'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'arolinks.com'))
+STREAM_API = (environ.get('STREAM_API', 'd0bf69b0d0bb82a10fa9ff8a1d0d1f2bcc4fcda6'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/How_2_Download_From_TeraBox/5722'))
 
 # Admins, Channels & Users
